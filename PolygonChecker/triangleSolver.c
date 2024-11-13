db7182b8b259503e9ab625992cd6e1bc9a2dca9f
johannTom <37807657+johannTom@users.noreply.github.com>
2024-11-13 14:56:47 -0500

    Fixed all the requirements for the triangle and its angles

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "triangleSolver.h"
 
-char* analyzeTriangle(int side1, int side2, int side3) {
-	char* result = "";
-	int tem1, tem2;
+#define PI  3.14
+//159265358979323846
 
-	int temMax = side1;
+char* analyzeTriangle(double side1, double side2, double side3) {
+	char* result = "";
+	double tem1, tem2;
+	double angle1, angle2, angle3;
+//	double A, B, C;
+
+	double temMax = side1;
 	if (temMax < side2)
 	{
 
@@ -37,18 +43,51 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 	else if (side1 == side2 && side1 == side3) {
 		result = "Equilateral triangle";
+		angle1 = 60;
+		angle2 = 60;
+		angle3 = 60;
+		printf(" The 3 angles to the triangle is %.2f %.2f %.2f  \n", angle1, angle2, angle3);
 
 	}
 	else if ((side1 == side2 && side1 != side3) ||
 		(side1 == side3 && side1 != side2) ||
 		(side2 == side3 && side2 != side1))
 	{
-		result = "Isosceles triangle";
+		
+		result = "Isosceles triangle";   // cos(c)  = a^2 +b^2 - c^2 /2ab
+		//A = acos(((side2 * side2) + (side3 * side3) - (side1 * side1)) / 2 * side2 * side3);
+		//B = acos(((side1 * side1) + (side3 * side3) - (side2 * side2)) / 2 * side1 * side3);
+		//C = acos(((side1 * side1) + (side2 * side2) - (side3 * side3)) / 2 * side1 * side2);
 
+		//angle1 = A * (180 / PI);
+		//angle2 = B * (180 / PI);
+		//angle3 = C * (180 / PI);
+		angle1 = calculateAngle(side1, side2, side3);
+		angle2 = calculateAngle(side2, side1, side3);
+		angle3 = calculateAngle(side3, side1, side2);
+
+		printf(" The 3 angles to the triangle is %.2lf %.2lf %.2lf  \n", angle1, angle2, angle3);
 	}
 	else {
 		result = "Scalene triangle";
+		/*A = acos(((side2 * side2) + (side3 * side3) - (side1 * side1)) / 2 * side2 * side3);
+		B = acos(((side1 * side1) + (side3 * side3) - (side2 * side2)) / 2 * side1 * side3);
+		C = acos(((side1 * side1) + (side2 * side2) - (side3 * side3)) / 2 * side1 * side2);*/
+		
+	/*	angle1 = A * (180 / PI);
+		angle2 = B * (180 / PI);
+		angle3 = C * (180 / PI);*/
+
+		angle1 = calculateAngle(side1, side2, side3);
+		angle2 = calculateAngle(side2, side1, side3);
+		angle3 = calculateAngle(side3, side1, side2);
+ 
+		printf(" The 3 angles to the triangle is %.2lf %.2lf %.2lf  \n", angle1, angle2, angle3);
 	}
 
 	return result;
+}
+
+double calculateAngle(double a, double b, double c) {
+	return acos((b * b + c * c - a * a) / (2 * b * c)) * (180.0 / PI);
 }
