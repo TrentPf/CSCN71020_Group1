1e2d597a6cb8b7eff9e10ab7acb1cf7534ac3ebc
Trent Pfeiffer <trentpf@hotmail.com>
2024-11-24 13:33:25 -0500

    Add initial test framework for triangle functionalities

--- PolygonChecker_Testing/PolygonChecker_Testing.cpp
+++ PolygonChecker_Testing/PolygonChecker_Testing.cpp
@@ -32,33 +32,86 @@ namespace PolygonCheckerTesting
 			//Tests Triangle is identified as invalid when two shortest side lengths are not greater than the largest using lengths 3.00, 4.00, 9.00
 
 			char* result = " ";
-			result = analyzeTriangle(3.00, 4.00, 0.00);
+			result = analyzeTriangle(3.00, 4.00, 9.00);
 
 			Assert::AreEqual("Not a triangle", result);
 		}
 		TEST_METHOD(TestTriangleIsEquilateral)
 		{
 			//Tests Triangle is identified as Equilateral when side lengths are 3.00, 3.00, 3.00
+
+			char* result = " ";
+			result = analyzeTriangle(3.00, 3.00, 3.00);
+
+			Assert::AreEqual("Equilateral triangle", result);
 		}
 		TEST_METHOD(TestTriangleIsIsosceles)
 		{
 			//Tests Triangle is identified as Isosceles when side lengths are 3.00, 2.00, 2.00
+
+			char* result = " ";
+			result = analyzeTriangle(3.00, 2.00, 2.00);
+
+			Assert::AreEqual("Isosceles triangle", result);
 		}
 		TEST_METHOD(TestTriangleIsScalene)
 		{
 			//Tests Triangle is identified as Scalene when side lenghs are 3.00, 4.00, 5.00
+
+			char* result = " ";
+			result = analyzeTriangle(3.00, 4.00, 5.00);
+
+			Assert::AreEqual("Scalene triangle", result);
 		}
 		TEST_METHOD(TestEquilateralInnerAngleCorrect)
 		{
 			//Tests inner angles are valid for Equilateral Triangle with lengths 3.00, 3.00, 3.00
+
+			double side1 = 3.00;
+			double side2 = 3.00;
+			double side3 = 3.00;
+
+			double angle1 = calculateAngle(side1, side2, side3);
+			double angle2 = calculateAngle(side2, side1, side3);
+			double angle3 = calculateAngle(side3, side1, side2);
+
+			double sumAngle = angle1 + angle2 + angle3;
+
+			Assert::AreEqual(180.0, sumAngle);
+
+
 		}
 		TEST_METHOD(TestIsoscelesInnerAngleCorrect)
 		{
 			//Tests inner angles are valid for Isosceles Triangle with lengths 3.00, 2.00, 2.00
+
+			double side1 = 3.00;
+			double side2 = 2.00;
+			double side3 = 2.00;
+
+			double angle1 = calculateAngle(side1, side2, side3);
+			double angle2 = calculateAngle(side2, side1, side3);
+			double angle3 = calculateAngle(side3, side1, side2);
+
+			double sumAngle = angle1 + angle2 + angle3;
+
+			Assert::AreEqual(180.0, sumAngle);
 		}
 		TEST_METHOD(TestScaleneInnerAngleCorrect)
 		{
 			//Tests inner angles are valid for Scalene Triangle with lengths 3.00, 4.00, 5.00
+
+			double side1 = 3.00;
+			double side2 = 4.00;
+			double side3 = 5.00;
+
+			double angle1 = calculateAngle(side1, side2, side3);
+			double angle2 = calculateAngle(side2, side1, side3);
+			double angle3 = calculateAngle(side3, side1, side2);
+
+			double sumAngle = angle1 + angle2 + angle3;
+
+			Assert::AreEqual(180.0, sumAngle);
 		}
 		TEST_METHOD(TestPolygonIsRectangle)
 		{
