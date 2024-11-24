c7c78264ba415c8b2f7849eaeb641f04ead58e8b
Trent Pfeiffer <trentpf@hotmail.com>
2024-11-24 15:12:14 -0500

    Add complete testing files with ERRORED linker

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -2,3 +2,10 @@
 void printWelcome();
 int printShapeMenu();
 double* getTriangleSides(double* triangleSides);
+char* analyzeTriangle(double side1, double side2, double side3);
+double calculateAngle(double a, double b, double c);
+double calculateDistance(double x1, double y1, double x2, double y2);
+bool isRectangle(double distances[], double diagonals[]);
+double calculateRectangleArea(double side1, double side2);
+double calculateRectanglePerimeter(double side1, double side2);
+char* analyzeTriangle(double side1, double side2, double side3);
